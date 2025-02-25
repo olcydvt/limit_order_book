a7e47cdd16b8b24c8ca0c866b9086cca285dc2d1
Olcay Davut Cabbas <olcay.d.cabbas@gmail.com>
2025-02-25 16:49:59 +0300

    Update limit_order_book.hpp

--- limit_order_book.hpp
+++ limit_order_book.hpp
@@ -1,100 +1,112 @@
 #pragma once
-#include <iostream>
+
+#include <set>
+#include <unordered_map>
 #include <map>
-#include <mutex>
+/*
 
-namespace limit_order {
+buy/long/+
+sell/short/-
+if the order is but then price will be (+) integer
+controversially if the order is sell then price will be (-) integer
+*/
 
-    class limit_order_book {
-    private:
-        using price = int;
-        using amount = int;
-        using order_id = int;
-        std::map<price, amount, std::greater<>> buy_orders;
-        std::map<price, amount> sell_orders;
+class order_book {
+    using order_volume = int;
+    using order_price = int;
+    using order_id = int;
+    using order_book_entry_iterator = std::map<order_price, std::pair<order_volume, order_id>>::iterator;
+    using order_book_entry = order_book_entry_iterator::reference;
 
-        // TODO: can be lock free
-        std::mutex buy_orders_mutex;
-        std::mutex sell_orders_mutex;
+    std::map<order_price, std::pair<order_volume, order_id>> sell_order_book_map;
+    std::map<order_price, std::pair<order_volume, order_id>, std::greater<int>> buy_order_book_map;
+    std::unordered_map<order_id, order_book_entry_iterator> order_id_to_book_map;
 
-    public:
-        enum class order_side : int {
-            buy = 0,
-            sell = 1
-        };
-
-        struct executed_order {
-            int traded_price;
-            int traded_amount;
-        };
-
-        // Add a buy order (price, amount)
-        // Accumulate amount at the same price
-        void add_buy_order(int price, int amount) {
-            std::lock_guard<std::mutex> lock(buy_orders_mutex);
-            buy_orders[price] += amount;
-        }
-
-        // Add a sell order (price, amount)
-        // Accumulate amount at the same price
-        void add_sell_order(int price, int amount) {
-            std::lock_guard<std::mutex> lock(sell_orders_mutex);
-            sell_orders[price] += amount;
-        }
-
-        void print_order_book() {
-            std::lock_guard<std::mutex> lock_sell(sell_orders_mutex);
-            std::cout << "Sell Orders:\n";
-            for (const auto& [price, amount]: sell_orders) {
-                std::cout << "  Price: " << price << ", Amount: " << amount << '\n';
+    inline void check_and_execute_trade(order_id id, order_price requested_price) {
+        order_book_entry_iterator order_book_entry_itr;
+        if (requested_price > 0) {
+            order_book_entry_itr = sell_order_book_map.begin();
+            if (order_book_entry_itr == sell_order_book_map.end()) {
+                return; // no sell trade keep waiting available sell limit
             }
-            std::lock_guard<std::mutex> lock_buy(buy_orders_mutex);
-            std::cout << "Buy Orders:\n";
-            for (const auto& [price, amount]: buy_orders) {
-                std::cout << "  Price: " << price << ", Amount: " << amount << '\n';
-            }
-        }
+            const order_price& booked_order_price = order_book_entry_itr->first;
+            order_volume& booked_order_volume = order_book_entry_itr->second.first;
+            order_id booked_order_id = order_book_entry_itr->second.second;
 
-        executed_order process_order(order_side _order_side, int price, int amount) {
-            int initial_amount  = amount;
-            if (_order_side == order_side::buy) {
-                std::lock_guard<std::mutex> lock_buy(buy_orders_mutex);
-                std::lock_guard<std::mutex> lock_sell(sell_orders_mutex);
-
-                while (!sell_orders.empty() && price >= sell_orders.begin()->first && amount > 0) {
-                    auto [sell_price, sell_amount] = *sell_orders.begin();
-                    if (amount >= sell_amount) {
-                        amount -= sell_amount;
-                        sell_orders.erase(sell_orders.begin());
-                    } else {
-                        sell_orders[sell_price] -= amount;
-                        amount = 0;
-                    }
+            if (requested_price >= booked_order_price) {
+                order_volume& requested_volume = buy_order_book_map[requested_price].first;
+                requested_volume -= booked_order_volume;
+                if (requested_volume > 0) { //partially fill & booked order also full filled
+                    order_id_to_book_map.erase(booked_order_id);
+                    sell_order_book_map.erase(booked_order_price);
+                    check_and_execute_trade(id, requested_price);
                 }
-                if (amount > 0)
-                    add_buy_order(price, amount); // Add remaining amount to buy orders
-            } else {
-                std::lock_guard<std::mutex> lock_buy(buy_orders_mutex);
-                std::lock_guard<std::mutex> lock_sell(sell_orders_mutex);
-                while (!buy_orders.empty() && price <= buy_orders.begin()->first && amount > 0) {
-                    auto [buy_price, buy_amount] = *buy_orders.begin();
-                    if (amount >= buy_amount) {
-                        amount -= buy_amount;
-                        buy_orders.erase(buy_orders.begin());
-                    } else {
-                        buy_orders[buy_price] -= amount;
-                        amount = 0;
-                    }
+                else if (requested_volume < 0) { //full fill & booked order partially filled
+                    booked_order_volume = -requested_volume;
+                    order_id_to_book_map.erase(id);
+                    buy_order_book_map.erase(requested_price);
                 }
-                if (amount > 0)
-                    add_sell_order(price, amount); // Add remaining amount to sell orders
+                else { //full fill & booked order also full filled
+                    order_id_to_book_map.erase(booked_order_id);
+                    sell_order_book_map.erase(booked_order_price);
+
+                    order_id_to_book_map.erase(id);
+                    buy_order_book_map.erase(requested_price);
+                }
+
             }
+        }
+        else {
+            order_book_entry_itr = buy_order_book_map.begin();
+            if (order_book_entry_itr == buy_order_book_map.end()) {
+                return; // no sell trade keep waiting available buy limit
+            }
+            const order_price& booked_order_price = order_book_entry_itr->first;
+            order_volume& booked_order_volume = order_book_entry_itr->second.first;
+            order_id booked_order_id = order_book_entry_itr->second.second;
+            order_price normilezed_price = -requested_price;
+            if (normilezed_price <= booked_order_price) {
+                order_volume& requested_volume = sell_order_book_map[normilezed_price].first;
+                requested_volume -= booked_order_volume;
+                if (requested_volume > 0) { //partially fill & booked order also full filled
+                    order_id_to_book_map.erase(booked_order_id);
+                    buy_order_book_map.erase(booked_order_price);
+                    check_and_execute_trade(id, requested_price);
+                }
+                else if (requested_volume < 0) { //full fill & booked order partially filled
+                    booked_order_volume = -requested_volume;
+                    order_id_to_book_map.erase(id);
+                    sell_order_book_map.erase(normilezed_price);
+                }
+                else { //full fill & booked order also full filled
+                    order_id_to_book_map.erase(booked_order_id);
+                    sell_order_book_map.erase(booked_order_price);
 
-            return executed_order{price, initial_amount - amount};
+                    order_id_to_book_map.erase(id);
+                    buy_order_book_map.erase(normilezed_price);
+                }
+            }
+        }
+    }
+
+public:
+
+    void place_order(order_id id, order_price price, order_volume volume ) {
+        if (price < 0) {
+            int sell_price = -price;
+            sell_order_book_map[sell_price].first += volume;
+            sell_order_book_map[sell_price].second = id;
+            order_id_to_book_map[id] = sell_order_book_map.find(sell_price);
+        }
+        else {
+            buy_order_book_map[price].first += volume;
+            buy_order_book_map[price].second = id;
+            order_id_to_book_map[id] = buy_order_book_map.find(price);
         }
 
-        void cancel_order (order_id _id) {
-            // TODO: query order, if exist remove from the book
-        }
-    };
-}
+        check_and_execute_trade(id, price);
+    }
+
+};
+
+
